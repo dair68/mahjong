6462bf509fff0f99caab899238bf00324dfbd3f9
dair68 <granthuang68@yahoo.com>
2020-05-07 19:29:06 -0700

    cleaned up credit code

--- creditswidget.h
+++ creditswidget.h
@@ -2,22 +2,44 @@
 #define CREDITSWIDGET_H
 
 #include <QWidget>
-#include <QFrame>
-
+#include <QLabel>
 
 class CreditsWidget : public QWidget {
     Q_OBJECT
 public:
-    //creates widget
+    //constructor
     //@param parent - pointer to parent widget
     CreditsWidget(QWidget* parent = nullptr);
 
-    //overriding paint event for background image
-    void paintEvent(QPaintEvent *event) override;
+    //overriding paint event to display background image
+    void paintEvent(QPaintEvent* event) override;
 
 signals:
     //emit when back to title button clicked
     void backButtonClicked();
+
+private:
+    //creates and positions credit text
+    void createCreditLabels();
+
+    //creates image credits
+    //returns pointer to label with credit text and links
+    QLabel* createImageCredits();
+
+    //create music credits
+    //returns pointer to label with credit text and links
+    QLabel* createMusicCredits();
+
+    //creates sound credits
+    //returns pointer to label with credit text and links
+    QLabel* createSoundCredits();
+
+    //creates programming credits
+    //returns pointer to label with credit text and links
+    QLabel* createProgrammingCredits();
+
+    //sets default stylesheet properties
+    void initializeStyleSheet();
 };
 
 #endif // CREDITSWIDGET_H
